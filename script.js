8cbfef95b8d0af7f33293f6c8627f4e1ba997e82
bernardkrehula <krehula.bernard456@gmail.com>
2025-01-20 19:43:06 +0100

    First change
    Improved function of Mark all as read button

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const read = document.querySelector('.notifications p');
 const list = document.querySelectorAll('.list-one li');
+const dot = document.querySelectorAll('.dot');
+const num = document.querySelector('.block');
 
 read.addEventListener('click', () => {
     read.style.opacity = '0.4';
+    num.innerHTML = 0;
     list.forEach((list) => {
         list.style.backgroundColor = 'white';
-    })
+    });
+    dot.forEach((dot) => {
+        dot.style.display = 'none';
+    });
 })
